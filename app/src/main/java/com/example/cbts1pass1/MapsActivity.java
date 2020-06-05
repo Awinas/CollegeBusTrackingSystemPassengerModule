c7c745be95bb89fd1d1fea53dfb2d5d342b0841a
sumanthsai1 <s.sumanthsai1998@gmail.com>
2020-06-05 10:08:00 +0530

    Initial commit

---
+++ app/src/main/java/com/example/cbts1pass1/MapsActivity.java
@@ -0,0 +1,112 @@
+package com.example.cbts1pass1;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.location.Address;
+import android.location.Geocoder;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.FragmentActivity;
+
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptor;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.io.IOException;
+import java.util.List;
+
+import static com.example.cbts1pass1.R.drawable.busicon;
+
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+    private LatLng latlng;
+    private Marker marker;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+
+    }
+
+
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+        DatabaseReference databasereferance = FirebaseDatabase.getInstance().getReference("Location");
+        final Geocoder geocoder = new Geocoder(getApplicationContext());
+
+        ValueEventListener listener = databasereferance.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                Double latitude = dataSnapshot.child("latitude").getValue(Double.class);
+                Double longitude = dataSnapshot.child("longitude").getValue(Double.class);
+
+                int height = 100;
+                int width = 100;
+                Bitmap b = BitmapFactory.decodeResource(getResources(),busicon);
+                Bitmap smallMarker = Bitmap.createScaledBitmap(b, width, height, false);
+                BitmapDescriptor smallMarkerIcon = BitmapDescriptorFactory.fromBitmap(smallMarker);
+
+                try{
+                    List<Address> addresses =
+                        geocoder.getFromLocation(latitude, longitude, 1);
+                String result= addresses.get(0).getLocality()+":";
+                result += addresses.get(0).getAdminArea()+":";
+
+                result += addresses.get(0).getCountryName();
+
+
+
+                LatLng latLng = new LatLng(latitude,longitude);
+                marker = mMap.addMarker(new MarkerOptions().position(latLng).title(result).icon(BitmapDescriptorFactory.fromBitmap(smallMarker)));
+                mMap.setMaxZoomPreference(20);
+                CameraUpdate cameraUpdate = CameraUpdateFactory.newLatLngZoom(latLng, 17);
+                mMap.animateCamera(cameraUpdate);
+                }
+                catch (IOException e) {
+                    e.printStackTrace();
+                }
+                /*if (marker != null){
+                    marker.remove();
+
+                    marker = mMap.addMarker(new MarkerOptions().position(latLng).title("Bus Location"));
+                    mMap.setMaxZoomPreference(20);
+                    CameraUpdate cameraUpdate = CameraUpdateFactory.newLatLngZoom(latLng, 17);
+                    mMap.animateCamera(cameraUpdate);
+                }
+                else {
+                    marker = mMap.addMarker(new MarkerOptions().position(latLng).title("Bus Location"));
+                    mMap.setMaxZoomPreference(20);
+                    CameraUpdate cameraUpdate = CameraUpdateFactory.newLatLngZoom(latLng, 17);
+                    mMap.animateCamera(cameraUpdate);
+                }
+*/
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+    }
+}
